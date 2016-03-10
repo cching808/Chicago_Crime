c41982b0e00ba0e2413779f76355f980da27ac6e
Corey Ching <cycching@ucdavis.edu>
2016-03-09 20:52:42 -0800

    working on it

---
+++ app/app/scripts/app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by coreyching on 3/9/16.
+ */
+var chicagoApp = angular.module('Chicago', [
+    'ngRoute',
+    'chicagoControllers'
+]);
+
+chicagoApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+        when('/', {
+            templateUrl: 'views/main-view.html',
+            controller: 'MainViewCtrl'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+}]);
